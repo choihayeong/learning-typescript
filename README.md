384fa1c7c6082b9ae7def5acc7b294709b55736d
Hayeong <hychoi318@gmail.com>
2023-11-17 18:03:09 +0900

    count-down까지 완료

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# learn-ts-with-gulp
+# learning-typescript
+
+### 의존 패키지 설치
+```bash
+npm install
+
+#or
+
+npm i
+```
+
+### 개발 서버 실행
+```bash
+npm run dev
+
+#or 
+
+gulp
+
+#or
+
+gulp dev
+```
+
+### 프로덕션 배포
+```bash
+npm run build
+
+#or
+
+gulp build
+```
--- src/assets/scripts/common.ts
+++ src/assets/scripts/common.ts
@@ -1,3 +1,4 @@
+import { convertTypeAcquisitionFromJson } from "typescript";
 import { random } from "./random";
 
 const rndOne : number = random(10);
@@ -162,22 +163,31 @@ casinoForm?.addEventListener("submit", getSubmittedForm);
 const countdownEl = document.querySelector('.countdown') as HTMLElement;
 
 const TARGET_DATE = countdownEl?.dataset.date;
+const CONTENTS = countdownEl?.dataset.contents;
 
 const SECOND_IN_MS : number = 1000;
 const MINUTE_IN_MS = SECOND_IN_MS * 60;
-const HOUR_IN_MS = MINUTE_IN_MS * 60;
+const HOUR_IN_MS = MINUTE_IN_MS * 60; 
 const DAY_IN_MS = HOUR_IN_MS * 24;
 
-const getCountDown = () => {
+let intervalId;
+
+const countDday = () => {
   const TODAY = +new Date();
   const TARGET = TARGET_DATE ? +new Date(TARGET_DATE) : TODAY; // TARGET_DATE가 null 이든 undefined 일때는 일단 default 값으로 TODAY로 설정함.........
 
-  let diff : number;
+  let diff : number = TARGET - TODAY;
 
-  if (TARGET_DATE) {
-    diff = TARGET - TODAY;
-  }
+  const DAYS = Math.floor(diff / DAY_IN_MS);
+  const HH = Math.floor((diff % DAY_IN_MS) / HOUR_IN_MS);
+  const MM = Math.floor((diff % HOUR_IN_MS) / MINUTE_IN_MS);
+  const SS = Math.floor((diff % MINUTE_IN_MS) / SECOND_IN_MS);
 
+  countdownEl.innerHTML = `
+    After ${DAYS}Days ${HH}Hours ${MM}Minutes ${SS}Seconds, <br />
+    It's <strong>${CONTENTS}</strong>
+  `
 }
 
-getCountDown();
+countdownEl ? intervalId = setInterval(countDday, SECOND_IN_MS) : clearInterval(intervalId);
+
